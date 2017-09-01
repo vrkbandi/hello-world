c63f379c834687d59aeab940fb3b2fb44a7b1878
Jay Snellen <jsnellen@jsu.edu>
2017-09-01 09:34:47 -0500

    Added f2()

--- Class1.java
+++ Class1.java
@@ -6,4 +6,7 @@ public class Class1 {
     private void f1() {
         // v1.0
     }
+    private void f2() {
+        // v2.0
+    }
 }
--- Class2.java
+++ Class2.java
@@ -6,4 +6,7 @@ public class Class2 {
     private void f1() {
         // v1.0
     }
+    private void f2() {
+        // v2.0
+    }
 }
