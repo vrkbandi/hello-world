dc748cd8637f5bb0c22bd1b0accefbad3d3929b9
Jay Snellen <jsnellen@jsu.edu>
2017-09-01 09:34:10 -0500

    Initial Commit

---
+++ Class1.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author Your Name
+ */
+public class Class1 {
+    private void f1() {
+        // v1.0
+    }
+}
---
+++ Class2.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author Your Name
+ */
+public class Class2 {
+    private void f1() {
+        // v1.0
+    }
+}
